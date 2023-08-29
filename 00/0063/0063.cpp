5a9359eac70c62b40f21d5a7bb1dafdb1a3e1a74
hiro1729 <93374538+hiro1729@users.noreply.github.com>
2023-08-30 06:06:56 +0900

    a

---
+++ 00/0063/0063.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int l, k;
+	cin >> l >> k;
+	cout << (l - 1) / (2 * k) * k << '\n';
+}
---
+++ 00/0063/0063.py
@@ -0,0 +1,2 @@
+l, k = map(int, input().split())
+print((l - 1) // (2 * k) * k)
